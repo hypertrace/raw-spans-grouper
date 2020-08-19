e910a71a0ba8987699797edec34c8239c1cf0fd1
samarth-gupta-traceable <samarth@traceable.ai>
2020-08-19 15:09:44 +0530

    Add span arrival time and trace creation time to DataflowMetric

--- raw-spans-grouper/src/main/java/org/hypertrace/core/rawspansgrouper/RawSpanToStructuredTraceAvroGroupAggregator.java
+++ raw-spans-grouper/src/main/java/org/hypertrace/core/rawspansgrouper/RawSpanToStructuredTraceAvroGroupAggregator.java
@@ -2,22 +2,31 @@ package org.hypertrace.core.rawspansgrouper;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import com.typesafe.config.Config;
+import io.micrometer.core.instrument.Timer;
 import org.apache.avro.generic.IndexedRecord;
 import org.apache.flink.api.common.functions.AggregateFunction;
 import org.hypertrace.core.datamodel.RawSpan;
 import org.hypertrace.core.datamodel.StructuredTrace;
 import org.hypertrace.core.datamodel.TimestampRecord;
+import org.hypertrace.core.datamodel.Timestamps;
 import org.hypertrace.core.datamodel.shared.DataflowMetric;
 import org.hypertrace.core.datamodel.shared.trace.StructuredTraceBuilder;
+import org.hypertrace.core.serviceframework.metrics.PlatformMetricsRegistry;
 
 public class RawSpanToStructuredTraceAvroGroupAggregator implements
     AggregateFunction<RawSpan, List<RawSpan>, StructuredTrace> {
 
   private double dataflowSamplingPercent = -1;
   private final String DATAFLOW_SAMPLING_PERCENT = "dataflow.metriccollection.sampling.percent";
+  private static final String SPAN_GROUPER_ARRIVAL_LAG = "rawspangrouper.arrival.lag";
+  private static final Timer spanGrouperArrivalTimer =
+      PlatformMetricsRegistry.registerTimer(SPAN_GROUPER_ARRIVAL_LAG, new HashMap<>());
 
   public RawSpanToStructuredTraceAvroGroupAggregator(Config config) {
     if (config.hasPath(DATAFLOW_SAMPLING_PERCENT)
@@ -53,15 +62,21 @@ public class RawSpanToStructuredTraceAvroGroupAggregator implements
       rawSpanList.add((RawSpan) r);
     }
 
-    TimestampRecord timestampRecord = null;
-    if (Math.random()*100 <= dataflowSamplingPercent) {
-      timestampRecord = new TimestampRecord();
-      timestampRecord.setName(DataflowMetric.CREATION_TIME.toString());
-      timestampRecord.setTimestamp(System.currentTimeMillis());
+    Timestamps timestamps = null;
+    if (!accumulator.isEmpty() && Math.random()*100 <= dataflowSamplingPercent) {
+      long currentTime = System.currentTimeMillis();
+      long firstSpanArrivalTime = accumulator.get(0).getReceivedTimeMillis();
+      spanGrouperArrivalTimer.record(currentTime - firstSpanArrivalTime, TimeUnit.MILLISECONDS);
+      Map<String, TimestampRecord> records = new HashMap<>();
+      records.put(DataflowMetric.SPAN_ARRIVAL_TIME.toString(),
+          new TimestampRecord(DataflowMetric.SPAN_ARRIVAL_TIME.toString(), firstSpanArrivalTime));
+      records.put(DataflowMetric.TRACE_CREATION_TIME.toString(),
+          new TimestampRecord(DataflowMetric.TRACE_CREATION_TIME.toString(), currentTime));
+      timestamps = new Timestamps(records);
     }
 
     return StructuredTraceBuilder
-        .buildStructuredTraceFromRawSpans(rawSpanList, traceId, customerId, timestampRecord);
+        .buildStructuredTraceFromRawSpans(rawSpanList, traceId, customerId, timestamps);
   }
 
   @Override
